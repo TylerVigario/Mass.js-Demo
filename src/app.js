c68944483ef8d3fc712de67f253c0e8e946a36d6
Tyler Vigario <tylervigario90@gmail.com>
2019-10-29 22:21:55 -0700

    Finished live editing

--- src/app.js
+++ src/app.js
@@ -7,42 +7,63 @@ const Mass = new MassJS(Units);
 var inputMass;
 var outputValue;
 var outputFormat;
-var outputUnits;
+var loaded = false;
+var errors;
 
 $(() => {
-    inputMass = $('#inputMass');
-    outputValue = $('#outputValue');
-    outputFormat = $('#outputFormat');
+    inputMass = document.getElementById('inputMass');
+    outputValue = document.getElementById('outputValue');
+    outputFormat = document.getElementById('outputFormat');
 
-    // eslint-disable-next-line no-undef
-    outputUnits = CodeMirror(document.getElementById('outputUnits'), {
+    CodeMirror(document.getElementById('outputUnits'), {
         lineNumbers: true,
         mode: {
             name: 'javascript',
             json: true
         },
         gutters: ['CodeMirror-lint-markers'],
-        lint: true,
+        lint: {
+            getAnnotations: jsonValidator,
+            async: true 
+        },
         value: JSON.stringify(Units, null, 4)
     });
 
-    inputMass.on('input', () => {
-        try {
-            let value = Mass.parse(inputMass.val());
+    inputMass.onkeyup = onInput;
 
-            if (typeof value === 'number') {
-                outputValue.val(value);
-                outputFormat.val(Mass.format(value));
-            } else {
-                resetOutput();
-            }
-        } catch (e) {
-            resetOutput();
-        }
-    });
+    loaded = true;
 });
 
+function onInput() {
+    try {
+        let value = Mass.parse(inputMass.value);
+
+        if (typeof value === 'number') {
+            outputValue.value = value;
+            outputFormat.value = Mass.format(value);
+        } else {
+            resetOutput();
+        }
+    } catch (e) {
+        resetOutput();
+    }
+}
+
 function resetOutput() {
-    outputValue.val('');
-    outputFormat.val('');
+    outputValue.value = '';
+    outputFormat.value = '';
+}
+
+function jsonValidator(cm, updateLinting, options) {
+    errors = CodeMirror.lint.json(cm, options);
+
+    updateLinting(errors);
+    
+    resetOutput();
+
+    if (loaded && errors.length === 0) {
+        Mass.units = JSON.parse(cm);
+
+        onInput();
+    }
 }

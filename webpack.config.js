a308550de2d59cb3055f8558d727bd2d9021feba
Tyler Vigario <TylerVigario90@gmail.com>
2020-03-05 14:55:19 -0800

    Updated build system

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,47 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: {
-        main: './src/app.js'
+        main: ['babel-polyfill', './src/app.js'],
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname) + '/dist',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
     },
     module: {
-        rules: [{
-            test: /\.(scss)$/,
-            use: [{
-                loader: 'style-loader',
+        rules: [
+            { 
+                test: /\.scss$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    { loader: 'sass-loader' }
+                ]
             }, {
-                loader: 'css-loader',
-            }, {
-                loader: 'postcss-loader',
-                options: {
-                    plugins: function () {
-                        return [
-                            require('autoprefixer')
-                        ];
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
                     }
                 }
-            }, {
-                loader: 'sass-loader'
-            }]
-        }]
+            }
+        ]
     }
 };
